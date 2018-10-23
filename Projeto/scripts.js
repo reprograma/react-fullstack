bf5efe5ad382b5a988131d1794581b1745af72b4
camilaibs <camilaibs@gmail.com>
2018-10-22 22:24:09 -0300

    aula(introducao):  criando component com jsx

--- Projeto/scripts.js
+++ Projeto/scripts.js
@@ -1,34 +1,12 @@
-// <a class="link" href="/conta">Criar uma conta</a>
+const linkCriarUmaConta = <a class="link" href="/conta">Criar uma conta</a>
+const linkFazerLogin =  <a class="link" href="/login">Fazer login</a>
 
-const linkCriarUmaConta = React.createElement('a', {
-  className: 'link', 
-  href: '/conta', 
-  children: 'Criar uma conta'
-})
-
-// <a class="link" href="/login">Fazer login</a>
-
-const linkFazerLogin =  React.createElement('a', {
-  className: 'link', 
-  href: '/login', 
-  children: 'Fazer login'
-})
-
-/*
-<form>
-  <a class="link" href="/conta">Criar uma conta</a>
-  <a class="link" href="/login">Fazer login</a>
-</form>
-*/
-
-const formularioLogin = React.createElement('form', {
-  children: [linkCriarUmaConta, linkFazerLogin]
-})
-
+const formularioLogin = (
+  <form>
+    {[linkCriarUmaConta, linkFazerLogin]}
+  </form>
+)
 
 const divisaoProjeto = document.getElementById('projeto')
 
-// ReactDOM.render(elementReact, tagHtml)
 ReactDOM.render(formularioLogin, divisaoProjeto)
-
-
