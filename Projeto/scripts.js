5d8e768534c7f62f3eb5b5088bd913246930fb67
camilaibs <camilaibs@gmail.com>
2018-10-23 21:19:17 -0300

    aula(introducao): criando primeiros componentes

--- Projeto/scripts.js
+++ Projeto/scripts.js
@@ -1,12 +1,39 @@
-const linkCriarUmaConta = <a class="link" href="/conta">Criar uma conta</a>
-const linkFazerLogin =  <a class="link" href="/login">Fazer login</a>
+function Link(props) {
+  return (
+    <a className="link" href={props.url}>
+      {props.children}
+    </a>
+  )
+}
 
-const formularioLogin = (
-  <form>
-    {[linkCriarUmaConta, linkFazerLogin]}
-  </form>
+function Botao(props) {
+  let classes = "botao"
+
+  if (props.desabilitado) {
+    classes += " botao--desabilitado"
+  }
+
+  return (
+    <button className={classes}>
+      {props.children}
+    </button>
+  )
+}
+
+const paginaLogin = (
+  <main className="pagina-login">
+    <h1>Login</h1>
+    <p>Entre com seu email e senha.</p>
+    <Link url="/conta">Criar uma conta</Link>
+    <Link url="/login">Fazer login</Link>
+    <Botao desabilitado>Enviar</Botao>
+    <Botao>Enviar</Botao>
+  </main>
 )
 
 const divisaoProjeto = document.getElementById('projeto')
+ReactDOM.render(paginaLogin, divisaoProjeto)
 
-ReactDOM.render(formularioLogin, divisaoProjeto)
+
+
+
