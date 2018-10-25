f4b914196f60a16329dfed3f0d47d4d5eac907d1
camilaibs <camilaibs@gmail.com>
2018-10-25 20:50:27 -0300

    aula(conceito): facilidade de reusar componentes

--- Projeto/src/index.js
+++ Projeto/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Login from './paginas/Login/Login';
+import Conta from './paginas/Conta/Conta';
 import './index.css'
 
 
-const pagina = <Login />
-
 const divisaoProjeto = document.getElementById('projeto')
 
-ReactDOM.render(pagina, divisaoProjeto)
+ReactDOM.render(<Conta />, divisaoProjeto)
---
+++ Projeto/src/paginas/Conta/Conta.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Link from '../../componentes/Link/Link'
+import Botao from '../../componentes/Botao/Botao'
+import Legenda from '../../componentes/Legenda/Legenda'
+import Campo from '../../componentes/Campo/Campo'
+import './Conta.css'
+
+function Conta() {
+  return (
+    <main className="conta">
+      <h1>Conta</h1>
+      <p>Envie o formulário para criar uma conta!</p>
+      
+      <Legenda htmlFor="nome">Nome:</Legenda>
+      <Campo id="nome" type="text" name="nome" placeholder="Nome"/>
+      
+      <Legenda htmlFor="telefone">Telefone:</Legenda>
+      <Campo id="telefone" type="tel" name="telefone" placeholder="Telefone"/>
+      
+      <Legenda htmlFor="email">Email:</Legenda>
+      <Campo id="email"type="email" name="email" placeholder="Email"/>
+      
+      <Legenda htmlFor="senha">Senha:</Legenda>
+      <Campo id="senha"type="password"name="senha" placeholder="Senha"/>
+      
+      <Botao>Enviar</Botao>
+
+      <Link url="/login">Fazer login</Link>
+    </main>
+  )
+}
+
+export default Conta
