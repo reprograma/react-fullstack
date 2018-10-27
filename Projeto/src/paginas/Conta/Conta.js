edecb768ab01a9b4b2327fec43cba908a291ae14
camilaibs <camilaibs@gmail.com>
2018-10-26 22:08:18 -0300

    aula(conceito): validacao de email

--- Projeto/src/paginas/Conta/Conta.js
+++ Projeto/src/paginas/Conta/Conta.js
@@ -12,16 +12,43 @@ function Conta() {
       <p>Envie o formulário para criar uma conta!</p>
       
       <Legenda htmlFor="nome">Nome:</Legenda>
-      <Campo id="nome" type="text" name="nome" placeholder="Nome" obrigatorio/>
+      <Campo 
+        id="nome" 
+        type="text" 
+        name="nome" 
+        placeholder="Nome" 
+        required
+        minLength={10}
+      />
       
       <Legenda htmlFor="telefone">Telefone:</Legenda>
-      <Campo id="telefone" type="tel" name="telefone" placeholder="Telefone"/>
+      <Campo 
+        id="telefone" 
+        type="tel" 
+        name="telefone" 
+        placeholder="Telefone"
+        required
+      />
       
       <Legenda htmlFor="email">Email:</Legenda>
-      <Campo id="email"type="email" name="email" placeholder="Email"/>
+      <Campo 
+        id="email" 
+        type="email" 
+        name="email"
+        placeholder="Email"
+        required
+        pattern={/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/}
+      />
       
       <Legenda htmlFor="senha">Senha:</Legenda>
-      <Campo id="senha"type="password"name="senha" placeholder="Senha"/>
+      <Campo 
+        id="senha" 
+        type="password" 
+        name="senha"
+        placeholder="Senha"
+        required
+        minLength={6}
+      />
       
       <Botao>Enviar</Botao>
 
