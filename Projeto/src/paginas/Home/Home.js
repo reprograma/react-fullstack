91f04461769bac4e33875691722de5075edd963e
camilaibs <camilaibs@gmail.com>
2018-11-07 22:24:18 -0200

    aula(redux): fazendo carregando na home

--- Projeto/src/paginas/Home/Home.js
+++ Projeto/src/paginas/Home/Home.js
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import carregando from './carregando.svg'
 import './Home.css'
 
-function Home(props) {
-  if (!props.usuario) {
-    return <Redirect to="/login" />
+class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { carregando: true }
   }
 
+  render() {
+    if (!this.props.usuario) {
+      return <Redirect to="/login" />
+    }
 
-  return (
-    <main className="home">
-
-    </main>
-  )
+    return (
+      <main className="home">
+        {this.state.carregando ? (
+          <img className="home__loading" src={carregando} alt="Carregando" />
+        ) : (
+          <div>
+            Aqui listarei os postits amanhã
+          </div>
+        )}
+      </main>
+    )
+  }
 }
 
 export default connect(
